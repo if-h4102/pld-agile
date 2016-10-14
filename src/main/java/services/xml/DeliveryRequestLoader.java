5d73199c44e2772dbb30fb2b0cf8efc0adfc94f0
Charles Samborski <demurgos.net@gmail.com>
2016-10-15 00:19:40 +0200

    Test if unit-tests have access to test-ressources (for example, to read XML samples)

--- src/main/java/services/xml/DeliveryRequestLoader.java
+++
@@ -1,4 +0,0 @@
-package services.xml;
-
-public class DeliveryRequestLoader {
-}
--- src/test/java/services/xml/ParserTest.java
+++ src/test/java/services/xml/ParserTest.java
@@ -1,13 +1,15 @@
 package services.xml;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.net.URL;
 
-public class CityMapLoaderTest {
+public class ParserTest {
     @Test()
     public void testParser() throws Exception {
         URL testMapPath = getClass().getResource("/services/xml/test-map.xml");
+        Assert.assertNotNull(testMapPath);
         // TODO: open, parse and compare with the expected result
     }
 }
