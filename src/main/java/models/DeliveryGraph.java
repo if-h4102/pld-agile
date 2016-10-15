797650fb6909fad46709020efb5a0f8f0bc0aef6
Sn0wFox <ruben.pericas@laposte.net>
2016-10-15 18:27:48 +0200

    Added DeliveryGraph basis

--- src/main/java/models/DeliveryGraph.java
+++ src/main/java/models/DeliveryGraph.java
@@ -1,8 +1,29 @@
 package models;
 
 public class DeliveryGraph {
-    // TODO
+
+    /**
+     * The matrix representing the complete graph.
+     */
+    private Route[][] routes;
+
+    /**
+     * Consruct a complete graph
+     * @param routes
+     */
+    public DeliveryGraph(Route[][] routes) {
+        // TODO: compute matrix here ?
+        this.routes = routes;
+    }
+
+    /**
+     * TODO: description
+     * @param startId
+     * @param endId
+     * @return
+     */
     public Route getRoute(int startId, int endId) {
-        return null;
+        // TODO: handle exceptions
+        return routes[startId][endId];
     }
 }
