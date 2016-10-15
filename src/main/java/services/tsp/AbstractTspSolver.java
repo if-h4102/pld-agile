fa70a81d58831e70aece1e7c84276dfe19ccb9eb
Sn0wFox <ruben.pericas@laposte.net>
2016-10-15 16:06:58 +0200

    Added abstract TSP solver

---
+++ src/main/java/services/tsp/AbstractTspSolver.java
@@ -0,0 +1,13 @@
+package services.tsp;
+
+import models.DeliveryGraph;
+import models.Planning;
+
+public abstract class AbstractTspSolver {
+    /**
+     * Solve the TSP problem from a DeliveryGraph.
+     * @param graph The (complete) graph representing all delivery points and the warehouse.
+     * @return The delivery plan (Planning) associated to the given DeliveryGraph.
+     */
+    public abstract Planning solve(DeliveryGraph graph);
+}
