ec88964d05044ab2b5442ea5c0f929d4a20057fc
Sn0wFox <ruben.pericas@laposte.net>
2016-10-22 11:16:01 +0200

    Added Planning .addWayPoint() and .removeWayPoint() templates

--- src/main/java/models/Planning.java
+++ src/main/java/models/Planning.java
@@ -4,7 +4,10 @@ import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Planning {
     final private SimpleListProperty<Route> routes = new SimpleListProperty<>();
@@ -28,6 +31,16 @@ public class Planning {
 
     public Planning(List<Route> routes) {
         this(FXCollections.observableArrayList(routes));  // Copy the values in `routes` to an ObservableList
+    }
+
+    public Planning addWayPoint(AbstractWayPoint point) {
+        // TODO
+        return this;
+    }
+
+    public Planning removeWayPoint(AbstractWayPoint point) {
+        // TODO
+        return this;
     }
 
     public SimpleListProperty<Route> getRoutes(){
