392ca4d440cc77ac56ed49b646cdee2b7dd3da0a
Sn0wFox <ruben.pericas@laposte.net>
2016-10-29 17:05:47 +0200

    Implemented ComandManager

--- src/main/java/services/command/CommandManager.java
+++ src/main/java/services/command/CommandManager.java
@@ -3,18 +3,49 @@ package services.command;
 import java.util.Stack;
 
 public class CommandManager {
+    /**
+     * The stack storing the already done commands.
+     */
     private Stack<AbstractCommand> done;
+
+    /**
+     * The stack storing the undone commands.
+     */
     private Stack<AbstractCommand> undone;
 
+    /**
+     * Construct a new CommandManager.
+     */
+    public CommandManager() {
+        this.done = new Stack<>();
+        this.undone = new Stack<>();
+    }
+
+    /**
+     * Execute the given command and store it for an eventual undo operation.
+     * @param command the command to be executed.
+     */
     public void execute(AbstractCommand command) {
-        // TODO
+        this.done.push(command).execute();
+        // TODO: add mechanism to tell if the command was successful or not ?
     }
 
+    /**
+     * Execute the reversed command associated to the last done command
+     * and store it for an eventual redo operation.
+     */
     public void undo() {
-        // TODO
+        this.undone.push(this.done.pop().getReversed()).execute();
+        // TODO: add mechanism to tell if the command was successful or not ?
+
     }
 
+    /**
+     * Execute the reversed command associated to the last undone command
+     * and store it for an eventual undo command.
+     */
     public void redo() {
-        // TODO
+        this.done.push(this.undone.pop().getReversed()).execute();
+        // TODO: add mechanism to tell if the command was successful or not ?
     }
 }
