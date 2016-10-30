27bb40fb5d9c62b3f9fbefbccc628395a2cc528b
Charles Samborski <demurgos.net@gmail.com>
2016-10-30 18:52:02 +0100

    Update GUI for planning details.
    Use VBox for nodes and add buttons to edit or remove a waypoint.

--- src/main/java/components/planningdetails/PlanningDetails.java
+++ src/main/java/components/planningdetails/PlanningDetails.java
@@ -1,28 +1,84 @@
 package components.planningdetails;
 
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import models.Intersection;
 import models.Route;
 
-//http://stackoverflow.com/questions/19588029/customize-listview-in-javafx-with-fxml
-public class PlanningDetails extends ListView<Route> {
+import java.io.IOException;
+
+public class PlanningDetails<E extends Route> extends ScrollPane {
+    @FXML
+    protected VBox vBox;
+    private SimpleListProperty<E> items;
+
     public PlanningDetails() {
         super();
-        String cssPath = getClass().getResource("/components/planningdetails/planningDetails.css").toExternalForm();
-        this.getStylesheets().add(cssPath);
 
-        setCellFactory(view -> new ListCell<Route>() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/components/planningdetails/PlanningDetails.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        observeItems();
+    }
+
+    public void observeItems () {
+        final ObservableList<Node> itemNodes = this.vBox.getChildren();
+
+        itemsProperty().addListener(new ListChangeListener<E>() {
             @Override
-            public void updateItem(Route newValue, boolean empty) {
-                super.updateItem(newValue, empty);
-                if (empty) {
-                    setGraphic(null);
-                } else {
-                    PlanningDetailsItem item = new PlanningDetailsItem();
-                    setGraphic(item);
-                    item.setRoute(newValue);
-                }
+            public void onChanged(Change<? extends E> change) {
+                refreshAll();
+//                while (change.next()) {
+//                    if (change.wasAdded()) {
+//                        itemNodes.add(new PlanningDetailsItem());
+//                    }
+//                }
             }
         });
+    }
+
+    private void refreshAll () {
+        final ObservableList<Node> itemNodes = this.vBox.getChildren();
+        itemNodes.clear();
+        final ObservableList<E> items = getItems();
+        if (getItems() == null) {
+            return;
+        }
+
+        int index = 0;
+        for (E item: items) {
+            final PlanningDetailsItem node = new PlanningDetailsItem();
+            node.setIndex(index++);
+            node.setItem(item);
+            itemNodes.add(node);
+        }
+    }
+
+    public final SimpleListProperty<E> itemsProperty() {
+        if (items == null) {
+            items = new SimpleListProperty<>(this, "items", null);
+        }
+        return items;
+    }
+
+    public final void setItems(ObservableList<E> items) {
+        itemsProperty().setValue(items);
+    }
+
+    public final ObservableList<E> getItems() {
+        return itemsProperty().getValue();
     }
 }
--- src/main/java/components/planningdetails/PlanningDetailsItem.java
+++ src/main/java/components/planningdetails/PlanningDetailsItem.java
@@ -1,22 +1,34 @@
 package components.planningdetails;
 
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
+import models.AbstractWayPoint;
+import models.Intersection;
 import models.Route;
 
 import java.io.IOException;
 
-class PlanningDetailsItem extends StackPane {
-    private static final Route DEFAULT_ROUTE = null;
+public class PlanningDetailsItem<E extends Route> extends AnchorPane {
     @FXML
     protected Label content;
 
-    private SimpleObjectProperty<Route> route;
+    private SimpleObjectProperty<E> item;
+    private SimpleIntegerProperty index;
+    private SimpleStringProperty waypointName;
+    private SimpleStringProperty coordinates;
 
-    PlanningDetailsItem() {
+    public PlanningDetailsItem() {
+        updateWaypointName();
+        updateCoordinates();
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/components/planningdetails/PlanningDetailsItem.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
@@ -27,28 +39,99 @@ class PlanningDetailsItem extends StackPane {
             throw new RuntimeException(exception);
         }
 
-        routeProperty().addListener(event -> content.setText(
-            "Duration: " + getRoute().getDuration() + ", Street Sections: " + getRoute().getStreetSections().size()
-            + ", Start: (" + getRoute().getStartWaypoint().getIntersection().getX()
-            + ", " + getRoute().getStartWaypoint().getIntersection().getY()
-            + "), End: (" + getRoute().getEndWaypoint().getIntersection().getX()
-            + ", " + getRoute().getEndWaypoint().getIntersection().getY()
-            + ")"
-        ));
+        indexProperty().addListener(event -> updateWaypointName());
+        itemProperty().addListener(event -> updateCoordinates());
     }
 
-    public final SimpleObjectProperty<Route> routeProperty() {
-        if (route == null) {
-            route = new SimpleObjectProperty<>(this, "route", DEFAULT_ROUTE);
+    // Item
+    public final SimpleObjectProperty<E> itemProperty() {
+        if (item == null) {
+            item = new SimpleObjectProperty<>(this, "item", null);
         }
-        return route;
+        return item;
     }
 
-    public final void setRoute(Route value) {
-        routeProperty().setValue(value);
+    public final void setItem(E value) {
+        itemProperty().setValue(value);
     }
 
-    public final Route getRoute() {
-        return route == null ? DEFAULT_ROUTE : routeProperty().getValue();
+    public final E getItem() {
+        return item == null ? null : itemProperty().getValue();
+    }
+
+    // Index
+    public final SimpleIntegerProperty indexProperty() {
+        if (index == null) {
+            index = new SimpleIntegerProperty(this, "index");
+        }
+        return index;
+    }
+
+    public final void setIndex(int value) {
+        indexProperty().setValue(value);
+    }
+
+    public final int getIndex() {
+        return indexProperty().getValue();
+    }
+
+    // WaypointName
+    public final SimpleStringProperty waypointNameProperty() {
+        if (waypointName == null) {
+            waypointName = new SimpleStringProperty(this, "waypointName");
+        }
+        return waypointName;
+    }
+
+    public final void setWaypointName(String value) {
+        waypointNameProperty().setValue(value);
+    }
+
+    public final String getWaypointName() {
+        return waypointNameProperty().getValue();
+    }
+
+    // Coordinates
+    public final SimpleStringProperty coordinatesProperty() {
+        if (coordinates == null) {
+            coordinates = new SimpleStringProperty(this, "coordinates");
+        }
+        return coordinates;
+    }
+
+    public final void setCoordinates(String value) {
+        coordinatesProperty().setValue(value);
+    }
+
+    public void updateCoordinates() {
+        final Route item = getItem();
+        if (item == null) {
+            return;
+        }
+        final AbstractWayPoint waypoint = item.getStartWaypoint();
+        if (waypoint == null) {
+            return;
+        }
+        final Intersection intersection = waypoint.getIntersection();
+        if (intersection == null) {
+            return;
+        }
+        setCoordinates("("+intersection.getX()+"; "+intersection.getY()+")");
+    }
+
+    public void updateWaypointName() {
+        setWaypointName("Waypoint #" + getIndex());
+    }
+
+    public final String getCoordinates() {
+        return coordinatesProperty().getValue();
+    }
+
+    public void onRemoveButtonAction(ActionEvent actionEvent) {
+        System.out.println("Remove waypoint ...");
+    }
+
+    public void onEditButtonAction(ActionEvent actionEvent) {
+        System.out.println("Edit waypoint ...");
     }
 }
