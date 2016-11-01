fdcff88e1777904dbce4f589c328d292a6ad554d
anliec <nicolas.six@pau.fr>
2016-11-01 20:48:18 +0100

    Update bound method, now use the fact that a node is linked to two differents node (in short) Bound also add a malus if a node can't be reached on time.

--- src/main/java/services/tsp/AbstractTspSolver.java
+++ src/main/java/services/tsp/AbstractTspSolver.java
@@ -24,5 +24,6 @@ public abstract class AbstractTspSolver {
                                                   Map<AbstractWayPoint, Map<AbstractWayPoint, Integer>> costs, Map<AbstractWayPoint, Integer> deliveryDurations);
 
     protected abstract int bound(AbstractWayPoint lastSeenNode, ArrayList<AbstractWayPoint> unseen,
-                      Map<AbstractWayPoint, Map<AbstractWayPoint, Integer>> costs, Map<AbstractWayPoint, Integer> deliveryDurations);
+                                 Map<AbstractWayPoint, Map<AbstractWayPoint, Integer>> costs, Map<AbstractWayPoint, Integer> deliveryDurations,
+                                 int seenCost);
 }
--- src/main/java/services/tsp/BasicBoundTspSolver.java
+++ src/main/java/services/tsp/BasicBoundTspSolver.java
@@ -24,21 +24,67 @@ public class BasicBoundTspSolver extends BasicTspSolver{
      */
     @Override
     protected int bound(AbstractWayPoint lastSeenNode, ArrayList<AbstractWayPoint> unseen,
-                        Map<AbstractWayPoint, Map<AbstractWayPoint, Integer>> costs, Map<AbstractWayPoint, Integer> deliveryDurations) {
+                        Map<AbstractWayPoint, Map<AbstractWayPoint, Integer>> costs,
+                        Map<AbstractWayPoint, Integer> deliveryDurations,
+                        int seenCost) {
 
-        //init bound with min cost to get out of lastSeenNode
-        Map<AbstractWayPoint,Integer> linkCosts = costs.get(lastSeenNode);
-        int bound = Collections.min(linkCosts.values());
+        //init bound
+        int bound;
+        {
+            //first get the min half cost out of lastSeenNode
+            int minCost = Integer.MAX_VALUE;
+            for (AbstractWayPoint possibleDestination : unseen) {
+                int cost = costs.get(lastSeenNode).get(possibleDestination);
+                if (minCost >= cost) {
+                    minCost = cost;
+                }
+            }
+            bound = minCost / 2;
+            //then get the min half cost to the warehouse
+            minCost = Integer.MAX_VALUE;
+            for (AbstractWayPoint possibleDestination : unseen) {
+                int cost = costs.get(possibleDestination).get(this.startPoint);
+                if (minCost >= cost) {
+                    minCost = cost;
+                }
+            }
+            bound += minCost / 2;
+        }
 
-        //add to bound the min cost of each unseen node
+        //add to bound the average of the two min cost of each unseen node and is duration
         for (AbstractWayPoint wayPoint : unseen) {
-            linkCosts = costs.get(wayPoint);
-            int minCost = linkCosts.get(this.startPoint); //init using the cost of the warehouse
-            int cost = Collections.min(linkCosts.values());
-            if(cost < minCost)
+            //linkCosts = costs.get(wayPoint);
+            int minCost = costs.get(wayPoint).get(this.startPoint); //init using the cost to the warehouse
+            int cost = costs.get(lastSeenNode).get(wayPoint); //first check the cost from the last seen node
+            int secondMinCost = cost;
+            if(minCost >= cost){
+                secondMinCost = minCost;
                 minCost = cost;
+            }
+            //find the min cost and the second min cost
+            for (AbstractWayPoint possibleDestination : unseen){
+                if(wayPoint != possibleDestination){
+                    //as we cannot go from and to the same node we take the min of the two possibilities
+                    int costFrom = costs.get(wayPoint).get(possibleDestination);
+                    int costTo = costs.get(possibleDestination).get(wayPoint);
+                    cost = Math.min(costFrom,costTo) ;
+                    if(minCost >= cost){
+                        secondMinCost = minCost;
+                        minCost = cost;
+                    }
+                    else if(secondMinCost > cost){
+                        secondMinCost = cost;
+                    }
+                }
+            }
+            //add found values to bound
             bound += wayPoint.getDuration();
-            bound += minCost;
+            bound += (minCost+secondMinCost)/2;
+
+            //if  endDeliveryTime is already passed, add a malus
+            int deliveryFirstPossibleTime = seenCost+costs.get(lastSeenNode).get(wayPoint);
+            if( !wayPoint.canBePassed(deliveryFirstPossibleTime) )
+                bound += 86400;
         }
         return bound; // Basic bound
     }
