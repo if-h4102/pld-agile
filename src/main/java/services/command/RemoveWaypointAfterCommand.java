54c5fbec6b705b060fabf23f79ccaca19bb8582d
Sn0wFox <ruben.pericas@laposte.net>
2016-11-08 09:37:11 +0100

    Updated package services.command accordingly to the new model

--- src/main/java/services/command/RemoveWaypointAfterCommand.java
+++ src/main/java/services/command/RemoveWaypointAfterCommand.java
@@ -1,7 +1,25 @@
 package services.command;
 
-/**
- * Created by Ruben on 08/11/2016.
- */
-public class RemoveWaypointAfterCommand {
+import models.AbstractWaypoint;
+import models.CityMap;
+import models.Planning;
+
+public class RemoveWaypointAfterCommand extends RemoveWaypointCommand {
+
+    private int index;
+
+    public RemoveWaypointAfterCommand(AbstractWaypoint waypointToAdd, int index, Planning planning, CityMap map) {
+        super(waypointToAdd, planning, map);
+        this.index = index;
+    }
+
+    @Override
+    public void execute() {
+        this.planning.removeWaypoint(this.waypointToRemove);
+    }
+
+    @Override
+    public AbstractCommand getReversed() {
+        return new AddWaypointAfterCommand(this.waypointToRemove, this.index, this.planning, this.map);
+    }
 }
