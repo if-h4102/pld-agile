0f214a0b00783e35c5bd771f2aa637df78256f7c
HugoMoy <hugo.moynac@gmail.com>
2016-11-08 09:07:50 +0100

    Intersection and delivery address cannot be see at the same time

--- src/main/java/components/mapcanvas/MapCanvas.java
+++ src/main/java/components/mapcanvas/MapCanvas.java
@@ -84,6 +84,8 @@ public class MapCanvas extends Canvas {
                             && eventY < delivery.getY() + DEFAULT_DELIVERY_SIZE / 2 && eventY > delivery.getY() - DEFAULT_DELIVERY_SIZE / 2) {
                             DeliverySelectionEvent deliver = new DeliverySelectionEvent(delivery, e.getX(), e.getY());
                             fireEvent(deliver);
+                            IntersectionSelectionEvent nullIntersect = new IntersectionSelectionEvent(null, e.getX(), e.getY());
+                            fireEvent(nullIntersect);
                             return;
                         }
                     }
@@ -94,6 +96,8 @@ public class MapCanvas extends Canvas {
                         && eventY < inter.getY() + DEFAULT_INTERSECTION_SIZE / 2 && eventY > inter.getY() - DEFAULT_INTERSECTION_SIZE / 2) {
                         IntersectionSelectionEvent intersect = new IntersectionSelectionEvent(inter, e.getX(), e.getY());
                         fireEvent(intersect);
+                        DeliverySelectionEvent nullDeliver = new DeliverySelectionEvent(null, e.getX(), e.getY());
+                        fireEvent(nullDeliver);
                         return;
                     }
                 }
