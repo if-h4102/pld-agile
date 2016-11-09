275f4408014415a8253009451c37870494a0c7f1
Charles Samborski <demurgos@demurgos.net>
2016-11-09 02:18:06 +0100

    Move draw functions from MapCanvas to MapRenderer

---
+++ src/main/java/services/map/MapRenderer.java
@@ -0,0 +1,197 @@
+package services.map;
+
+import com.google.java.contract.Requires;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import models.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class MapRenderer {
+    public static final int MARGIN_ERROR = 0;
+    public static final double INTERSECTION_SIZE = 10;
+    public static final double WAYPOINT_SIZE = 18;
+
+    private CityMap cityMap;
+    private DeliveryRequest deliveryRequest;
+    private Planning planning;
+
+    public static HashMap<StreetSection, Integer> getSectionMap(Iterable<Route> listRoutes) {
+        HashMap<StreetSection, Integer> sectionMap = new HashMap<StreetSection, Integer>();
+
+        for (Route route : listRoutes) {
+            List<StreetSection> streetSections = route.getStreetSections();
+            for (StreetSection section : streetSections) {
+                if (!sectionMap.containsKey(section)) {
+                    sectionMap.put(section, 1);
+                } else {
+                    sectionMap.put(section, sectionMap.get(section) + 1);
+                }
+            }
+        }
+        return sectionMap;
+    }
+
+    public static Color getColor(double step, double nbStep) {
+        double red = 0;
+        double green = 0;
+        double blue = 0;
+
+        if (3 * step < nbStep) {
+            red = 1 - (3 * step / nbStep);
+            green = 3 * step / nbStep;
+        } else if (3 * step < 2 * nbStep) {
+            green = 1 - (step / (2 * nbStep / 3));
+            blue = step / (2 * nbStep / 3);
+        } else {
+            red = step / (nbStep);
+            blue = 1 - (step / (nbStep));
+
+        }
+        return new Color(red, green, blue, 1);
+    }
+
+    public MapRenderer() {
+    }
+
+    @Requires("cityMap != null")
+    public void drawCityMap(GraphicsContext gc, CityMap cityMap) {
+        gc.setFill(Color.BLACK);
+        List<StreetSection> streetSections = cityMap.getStreetSections();
+        for (StreetSection section : streetSections) {
+            gc.setLineWidth(2);
+            gc.setStroke(Color.GREY);
+            gc.strokeLine(section.getStartIntersection().getX(), section.getStartIntersection().getY(),
+                section.getEndIntersection().getX(), section.getEndIntersection().getY());
+        }
+
+        List<Intersection> intersections = cityMap.getIntersections();
+        for (Intersection inter : intersections) {
+            gc.fillOval(inter.getX() - INTERSECTION_SIZE / 2, inter.getY() - INTERSECTION_SIZE / 2,
+                INTERSECTION_SIZE, INTERSECTION_SIZE);
+        }
+    }
+
+    @Requires("deliveryRequest != null")
+    public void drawDeliveryRequest(GraphicsContext gc, DeliveryRequest deliveryRequest) {
+        for (DeliveryAddress deliveryAddress : deliveryRequest.getDeliveryAddresses()) {
+            this.drawWaypoint(gc, deliveryAddress);
+        }
+        this.drawWaypoint(gc, deliveryRequest.getWarehouse());
+    }
+
+    @Requires("planning != null")
+    public void drawPlanning(GraphicsContext gc, Planning planning) {
+        Color colorStart = Color.BLUE;
+        Color currentColor = colorStart;
+
+        Iterable<Route> listRoutes = planning.getRoutes();
+
+        //Map containing the number of each streetSection crossed
+        HashMap<StreetSection, Integer> sectionMap = getSectionMap(listRoutes);
+
+        //actual section
+        int countSections = 1;
+        //total number of sections
+        int totalSections = 0;
+        //Count the total numbers of section
+        for (Route route : listRoutes) {
+            totalSections += route.getStreetSections().size();
+        }
+
+        //Stroke the line and the arrow
+        for (Route route : listRoutes) {
+            gc.setStroke(currentColor);
+            List<StreetSection> streetSections = route.getStreetSections();
+
+            for (StreetSection section : streetSections) {
+                gc.setLineWidth(4);
+                currentColor = getColor(countSections++, totalSections);
+                gc.setStroke(currentColor);
+                int passings = sectionMap.get(section);
+                if (passings > 1) {
+                    gc.strokeLine(section.getStartIntersection().getX() - 2 * passings, section.getStartIntersection().getY() - 2 * passings,
+                        section.getEndIntersection().getX() - 2 * passings, section.getEndIntersection().getY() - 2 * passings);
+                    sectionMap.put(section, passings - 1);
+                } else {
+                    gc.strokeLine(section.getStartIntersection().getX(), section.getStartIntersection().getY(),
+                        section.getEndIntersection().getX(), section.getEndIntersection().getY());
+                }
+                drawArrowBetweenStreetSection(gc, section, currentColor);
+            }
+        }
+
+        // Draw the active waypoints (above the roads):
+        for (AbstractWaypoint waypoint : planning.getWaypoints()) {
+            this.drawWaypoint(gc, waypoint);
+        }
+
+        // Draw order of delivery
+        int number = 1;
+        for (Route route : listRoutes) {
+            gc.setLineWidth(3);
+            gc.setStroke(Color.BLACK);
+            gc.strokeText("" + number, route.getStartWaypoint().getIntersection().getX(), route.getStartWaypoint().getIntersection().getY());
+            gc.setStroke(Color.WHITE);
+            gc.setLineWidth(1);
+            gc.strokeText("" + number, route.getStartWaypoint().getIntersection().getX(), route.getStartWaypoint().getIntersection().getY());
+            number++;
+        }
+    }
+
+    public void drawArrowBetweenStreetSection(GraphicsContext gc, StreetSection street, Color color) {
+        double xStart = street.getStartIntersection().getX();
+        double xEnd = street.getEndIntersection().getX();
+        double yStart = street.getStartIntersection().getY();
+        double yEnd = street.getEndIntersection().getY();
+
+        double alpha = Math.atan2(yStart - yEnd, xStart - xEnd);
+//        System.out.println("alpha = " +alpha);
+
+        double xthird = (xStart + 2 * xEnd) / 3;
+        double ythird = (yStart + 2 * yEnd) / 3;
+
+        double lengthCross = 10;
+        double xCross1 = xthird + lengthCross * Math.cos(alpha + Math.PI / 6);
+        double yCross1 = ythird + lengthCross * Math.sin(alpha + Math.PI / 6);
+        double xCross2 = xthird + lengthCross * Math.cos(alpha - Math.PI / 6);
+        double yCross2 = ythird + lengthCross * Math.sin(alpha - Math.PI / 6);
+
+        gc.setStroke(color);
+        gc.setLineWidth(2);
+        gc.strokeLine(xthird, ythird, xCross1, yCross1);
+        gc.strokeLine(xthird, ythird, xCross2, yCross2);
+    }
+
+    private void drawWaypoint(GraphicsContext gc, DeliveryAddress deliveryAddress) {
+        gc.setFill(Color.BLUE);
+        gc.fillOval(
+            deliveryAddress.getIntersection().getX() - WAYPOINT_SIZE / 2,
+            deliveryAddress.getIntersection().getY() - WAYPOINT_SIZE / 2,
+            WAYPOINT_SIZE,
+            WAYPOINT_SIZE
+        );
+    }
+
+    private void drawWaypoint(GraphicsContext gc, Warehouse warehouse) {
+        gc.setFill(Color.RED);
+        gc.fillOval(
+            warehouse.getIntersection().getX() - WAYPOINT_SIZE / 2,
+            warehouse.getIntersection().getY() - WAYPOINT_SIZE / 2,
+            WAYPOINT_SIZE,
+            WAYPOINT_SIZE
+        );
+    }
+
+    private void drawWaypoint(GraphicsContext gc, AbstractWaypoint waypoint) {
+        if (waypoint instanceof DeliveryAddress) {
+            this.drawWaypoint(gc, (DeliveryAddress) waypoint);
+        } else if (waypoint instanceof Warehouse) {
+            this.drawWaypoint(gc, (Warehouse) waypoint);
+        } else {
+            System.err.println("Trying to draw unknown waypoint:");
+            System.err.println(waypoint);
+        }
+    }
+}
