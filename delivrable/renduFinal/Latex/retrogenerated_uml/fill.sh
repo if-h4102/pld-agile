71dcf4dd91dc1a519653f7836e0b939b9ccbb92f
anliec <nicolas.six@pau.fr>
2016-11-10 20:42:41 +0100

    generate uml

---
+++ delivrable/renduFinal/Latex/retrogenerated_uml/fill.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+umls=$(find ../../../../src -name "*.png")
+
+latexFile="uml.tex"
+
+echo "%generated tex file" >$latexFile
+
+for uml in $umls; do
+	cp $uml ${uml##*/}
+	
+	echo "\begin{figure}[h!]
+    		\begin{center}
+      			\includegraphics[width=\linewidth]{retrogenerated_uml/${uml##*/}}
+      			\caption{Diagramme de classe du package ${uml##*/}}
+    		\end{center}
+  	\end{figure}">>$latexFile
+done
