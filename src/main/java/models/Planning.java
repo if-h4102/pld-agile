5ca307da775006cc20bf211c49d7244e24d22663
Sn0wFox <ruben.pericas@laposte.net>
2016-11-10 17:34:56 +0100

    Added Planning doc

--- src/main/java/models/Planning.java
+++ src/main/java/models/Planning.java
@@ -7,7 +7,6 @@ import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.TransformationList;
 import java.util.*;
 
 public class Planning {
@@ -32,15 +31,19 @@ public class Planning {
      */
     final private ReadOnlyListWrapper<PlanningWaypoint> planningWaypoints = new ReadOnlyListWrapper<>(this, "planningWaypoints");
 
+    /**
+     * The map of waiting times for all waypoints.
+     */
     final private Map<AbstractWaypoint, Integer> waypointWaitingTime;
 
+    /**
+     * The total amount of time need to complete all deliveries.
+     */
     private int fullTime;
 
     /**
      * Construct a new Planning based on the given sorted list of waypoints.
-     *
-     * @param waypoints
-     *            a sorted list of waypoints.
+     * @param waypoints a sorted list of waypoints.
      */
     public Planning(CityMap cityMap, Collection<AbstractWaypoint> waypoints, Map<AbstractWaypoint, Integer> waitingTimes, int fullTime) {
         this.cityMap.setValue(cityMap);
@@ -51,52 +54,46 @@ public class Planning {
         this.updatePlanningWaypoints();
     }
 
+    /**
+     * Get the city map property of the current planning.
+     * @return the city map property (javafx).
+     */
     public ReadOnlyObjectProperty<CityMap> cityMapProperty() {
         return this.cityMap.getReadOnlyProperty();
     }
 
+    /**
+     * Get the city map associated to the current planning.
+     * @return the city map associated to the curent planning.
+     */
     public CityMap getCityMap() {
         return this.cityMap.getValue();
     }
 
     /**
      * Get the total amount of time that the delivery man will need to complete all deliveries and loads.
-     *
      * @return the total amount of time needed to complete all deliveries and loads.
      */
-    // The full time is now a parameter of planning, so the penalty are in it
     public int getFullTime() {
-        // int fullTime = 0;
-        // for (Route r : this.routes) {
-        // fullTime += r.getDuration();
-        // fullTime += r.getStartWaypoint().getDuration();
-        // }
-        // for (int waitingTime : waypointWaitingTime.values()) {
-        // fullTime += waitingTime;
-        // }
-        return fullTime;
+        return this.fullTime;
     }
 
 
     /**
      * Get the time that the delivery man must wait at the given waypoint
-     *
-     * @param waypoint
-     *            The wait point where the delivery man will wait
+     * @param waypoint The waypoint where the delivery man will wait.
      * @return The waiting time of the delivery man
      */
     public int getWaitingTimeAtWaypoint(AbstractWaypoint waypoint) {
-        if (waypointWaitingTime.containsKey(waypoint)) {
+        if (this.waypointWaitingTime.containsKey(waypoint)) {
             // Avoid the nullPointerException if the waypoint is not in the map
-            return waypointWaitingTime.get(waypoint);
+            return this.waypointWaitingTime.get(waypoint);
         }
-
         return 0;
     }
 
     /**
      * Get all the waypoints of the current planning, wrapped for JavaFX.
-     *
      * @return the current sorted list of routes.
      */
     public ReadOnlyListProperty<AbstractWaypoint> waypointsProperty() {
@@ -105,7 +102,6 @@ public class Planning {
 
     /**
      * Get all the waypoints of the current planning.
-     *
      * @return the current sorted list of routes.
      */
     public ObservableList<AbstractWaypoint> getWaypoints() {
@@ -114,7 +110,6 @@ public class Planning {
 
     /**
      * Get all the routes of the current planning, wrapped for JavaFX.
-     *
      * @return the current sorted list of routes.
      */
     public ReadOnlyListProperty<Route> routesProperty() {
@@ -123,20 +118,22 @@ public class Planning {
 
     /**
      * Get all the waypoints with metadata of the current planning
-     *
      * @return the current sorted list of routes.
      */
     public ReadOnlyListProperty<PlanningWaypoint> planningWaypointsProperty() {
         return this.planningWaypoints.getReadOnlyProperty();
     }
 
+    /**
+     * Get the observable list of waypoints used by the planning.
+     * @return an observable list of waypoints used by the planning.
+     */
     public ObservableList<PlanningWaypoint> getPlanningWaypoints() {
         return this.planningWaypointsProperty().getValue();
     }
 
     /**
      * Get all the routes of the current planning.
-     *
      * @return the current sorted list of routes.
      */
     public ObservableList<Route> getRoutes() {
@@ -144,12 +141,12 @@ public class Planning {
     }
 
     /**
-     * Add a waypoint to the current planning, and update the current routes consequently.
-     *
-     * @param point
-     *            the waypoint to add to the current planning.
+     * Add a waypoint to the current planning at the best position possible,
+     * and update the current routes consequently.
+     * @param point the waypoint to add to the current planning.
      * @return the updated current planning.
      */
+    @Deprecated
     public Planning addWaypoint(AbstractWaypoint point) {
         // Compute the best position to introduce the given waypoint
         int bestTime = Integer.MAX_VALUE;
@@ -173,18 +170,12 @@ public class Planning {
         this.routes.add(bestPosition, newRoutes[0]);
 
         return this;
-
-        // TODO: refactor this with the following method
     }
 
     /**
      * Add a waypoint to the current planning after the given waypoint, and update the current routes consequently.
-     *
-     * @param waypoint
-     *            The waypoint to add to the current planning.
-     * @param index
-     *            The index where to add the waypoint. The current waypoint at this index and the following waypoints will be moved by one.
-     * @return the updated current planning.
+     * @param waypoint The waypoint to add to the current planning.
+     * @param index The index where to add the waypoint. The current waypoint at this index and the following waypoints will be moved by one.
      */
     @Requires({ "!this.waypoints.contains(waypoint)" })
     public void addWaypoint(AbstractWaypoint waypoint, int index) {
@@ -195,13 +186,10 @@ public class Planning {
 
     /**
      * Add a waypoint to the current planning after the given waypoint, and update the current routes consequently.
-     *
-     * @param waypoint
-     *            the waypoint to add to the current planning.
-     * @param afterWaypoint
-     *            the waypoint after which the new waypoint must be added.
-     * @return the updated current planning.
+     * @param waypoint the waypoint to add to the current planning.
+     * @param afterWaypoint the waypoint after which the new waypoint must be added.
      */
+    @Deprecated
     @Requires({ "!this.waypoints.contains(waypoint)", "this.waypoints.contains(afterWaypoint)" })
     public void addWaypoint(AbstractWaypoint waypoint, AbstractWaypoint afterWaypoint) {
         int index = this.waypoints.indexOf(afterWaypoint) + 1;
@@ -211,11 +199,9 @@ public class Planning {
     }
 
     /**
-     * Remove a waypoint to the current planning, and update the current routes consequently. Please not that the removed waypoint can't be
-     * the first warehouse.
-     *
-     * @param waypoint
-     *            The waypoint to remove from the current planning.
+     * Remove a waypoint to the current planning, and update the current routes consequently.
+     * Please not that the removed waypoint can't be the first warehouse.
+     * @param waypoint The waypoint to remove from the current planning.
      */
     @Requires({ "this.waypoints.contains(waypoint)", "!waypoint.equals(this.waypoints.get(0))" })
     public void removeWaypoint(AbstractWaypoint waypoint) {
@@ -229,14 +215,32 @@ public class Planning {
         // this.updateRoutes();
     }
 
+    /**
+     * Get the waypoint of the planning at the given index.
+     * The waypoints list is circular, so giving an index greater than the size of the list
+     * will still return a waypoint.
+     * @param index the index at which the wanted waypoint is supposed to be.
+     * @return the waypoint at the given index.
+     */
     public AbstractWaypoint getWaypoint(int index) {
-        int size = waypoints.size();
-        return waypoints.get(index % size);
+        return waypoints.get(index % this.waypoints.size());
     }
 
+    /**
+     * Get the warehouse with which the delivery man will start delivering things.
+     * @return the warehouse associated to the planning.
+     */
+    public Warehouse getWarehouse() {
+        return (Warehouse) this.getWaypoint(0);
+    }
+
+    /**
+     * Compute all routes used by the current planning.
+     * @return the list of all routes in the planning.
+     */
     protected List<Route> computeRoutes() {
         List<Route> result = new LinkedList<>();
-        for (int i = 0; i < waypoints.size(); i++) {
+        for (int i = 0; i < this.waypoints.size(); i++) {
             AbstractWaypoint startWaypoint = this.getWaypoint(i);
             AbstractWaypoint endWaypoint = this.getWaypoint(i + 1);
             result.add(getCityMap().shortestPath(startWaypoint, endWaypoint));
@@ -244,10 +248,17 @@ public class Planning {
         return result;
     }
 
+    /**
+     * Update the list of all routes used by the planning.
+     */
     protected void updateRoutes() {
         this.routes.setValue(FXCollections.observableArrayList(computeRoutes()));
     }
 
+    /**
+     * Transform all waypoints used by the planning into PlanningWayPoint objects.
+     * @return the list off all used waypoints in the shape of PlanningWayPoints.
+     */
     protected List<PlanningWaypoint> computePlanningWaypoints() {
         List<PlanningWaypoint> result = new ArrayList<>();
 
@@ -256,8 +267,7 @@ public class Planning {
 
         result.add(new PlanningWaypoint(time, time, warehouse, time, time, true));
 
-        for (int i = 0; i < this.routes.size(); i++) {
-            Route route = this.routes.get(i);
+        for (Route route: this.routes) {
             AbstractWaypoint target = route.getEndWaypoint();
             int startTime = time;
             time += route.getDuration();
@@ -278,15 +288,19 @@ public class Planning {
         return result;
     }
 
+    /**
+     * Update all waypoints used by the planning.
+     */
     protected void updatePlanningWaypoints() {
         this.updatePlanningWaypoints(0);
     }
 
+    /**
+     * Update all waypoints used by the planning, starting with the waypoint at the given index.
+     * @param startIndex the index of the first waypoint to update.
+     */
     protected void updatePlanningWaypoints(int startIndex) {
         this.planningWaypoints.setValue(FXCollections.observableArrayList(this.computePlanningWaypoints()));
     }
 
-    public Warehouse getWarehouse() {
-        return (Warehouse) this.getWaypoint(0);
-    }
 }
